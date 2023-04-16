a00fcc73982a7680ff1dd990c246bfb8db3b25eb
samad20 <musab.aljahdhami@gmail.com>
2023-04-16 12:05:37 +0400

    Using yield return to filter

---
+++ 5_Using_yield_return_to_filter/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var n in Filter(NaturalNumber(), x=> x % 1000==0))
+        {
+
+            Console.WriteLine(n);
+        }
+    }
+    public static IEnumerable<long> NaturalNumber()
+    {
+        long n = 0;
+
+        while (n < long.MaxValue)
+        {
+            yield return n;
+            n += 1;
+        }
+    }
+    public static IEnumerable<long> Filter(IEnumerable<long> nums, Func<long, bool> filterfunc)
+    {
+        foreach (var num in nums) { 
+        
+            if (filterfunc(num)) yield return num;
+        }
+
+    }
+}
