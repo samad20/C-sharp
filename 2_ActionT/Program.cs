26a38d6965a60de0afa7d6288cc019b56a0c847b
samad20 <musab.aljahdhami@gmail.com>
2023-04-16 12:01:33 +0400

    Action T

---
+++ 2_ActionT/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main(string[] args){
+        DoLongRunningThing(s=>Console.WriteLine(s));
+
+    }
+
+    static void DoLongRunningThing(Action<string> feedback){
+        for(int i=1; i<=10; i++){
+            ProcessOperation(i);
+            feedback($"Completed {i} of 10");
+        }
+    }
+    private static void ProcessOperation(in int i){
+        Thread.Sleep((int)Math.Sqrt(i*200000));
+    }
+}
